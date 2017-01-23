113fee09ff7154a9519fb21d8e8a8b66d935b602
emonzain <emonzain@gmail.com>
2017-01-23 17:16:33 +0100

    Update piprocessors-action.sh

--- shell-scripts/piprocessors-action.sh
+++ shell-scripts/piprocessors-action.sh
@@ -15,8 +15,7 @@
 #
 stop_all()
 {
-    echo "Stopping all processes";
-    
+    echo "=> Stopping all processes" | tee -a /var/www/html/webcontrol-limo/actions_loging.txt
 }
 
 #
@@ -24,21 +23,21 @@ stop_all()
 #
 start_process()
 {
-    echo "Starting process $1"    
+    echo "=> Starting process $1" | tee -a /var/www/html/webcontrol-limo/actions_loging.txt
 }
 
                                                                                                                                                               
     case "$2" in
     start)
-      echo "Starting pyLoad."
+      echo "Starting pyLoad." | tee -a /var/www/html/webcontrol-limo/actions_loging.txt
       start_process $1
     ;;
     stop)
-      echo "Shutting down pyLoad."
+      echo "Shutting down pyLoad." | tee -a /var/www/html/webcontrol-limo/actions_loging.txt
       stop_all
     ;;
     *)
-      echo "Usage: $0 {process_target} {start|stop}"
+      echo "Usage: $0 {process_target} {start|stop}" | tee -a /var/www/html/webcontrol-limo/actions_loging.txt
       exit 1
     esac
  
