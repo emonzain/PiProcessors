309dd0386130e1f4042ef098166bb0fdc51b120c
emonzain <emonzain@gmail.com>
2017-01-23 13:58:38 +0100

    Test apIresult

--- src/piprocess.php
+++ src/piprocess.php
@@ -15,25 +15,34 @@ class PiProcess {
 	function do_action($action)
 	{
 		$result = false;
+		$details = "";
 		switch($action)
 		{
 			case "on" : 
-				exec("sudo echo \"Starting process : ".$this->ProcessName." ... Result : OK\" | sudo tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
+				$details = exec("sudo echo \"Starting process : ".$this->ProcessName." ... Result : OK\" | sudo tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
 				$result = true;
 				break;
 				
 			case "off" : 
-				exec("sudo echo \"Stopping process : ".$this->ProcessName." ... Result : OK\" | sudo tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
+				$details = exec("sudo echo \"Stopping process : ".$this->ProcessName." ... Result : OK\" | sudo tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
 				$result = true;
 				break;
 			
 			default: 
-				exec("sudo echo \"Wrong action : ".$this->ProcessName." ... Result : Error\" | sudo tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
+				$details = exec("sudo echo \"Wrong action : ".$this->ProcessName." ... Result : Error\" | sudo tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
 				$result = false;
 				break;
 		}
 		
-		return $result;
+		
+		$apiResult = new ApiResult();
+		$apiResult->Result = $result;
+		$apiResult->Item = $result;
+		$apiResult->Details = $details;
+		
+		
+		return $apiResult;
+		
 	}
 } 
 
