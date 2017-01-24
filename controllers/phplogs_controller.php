7686b8810834073271d0b9f62c9728a784cd1cde
emonzain <emonzain@gmail.com>
2017-01-24 12:33:41 +0100

    Update phplogs_controller.php

--- controllers/phplogs_controller.php
+++ controllers/phplogs_controller.php
@@ -2,10 +2,17 @@
 class PhpLogsReader
 {
   static public function ShowPhpLogs()
-  {    
+  {   
+
     $html = "<h1>Logs Php</h1><br />";
-    $html = $html.file_get_contents('/var/www/html/webcontrol-limo/PiProcessors/php5-fpm.log');
     
-    return $html;
+    
+		$data = explode("\n", file_get_contents('/var/www/html/webcontrol-limo/PiProcessors/php5-fpm.log'));
+		foreach($data AS $line)
+		{
+			$html = $html.$line."<br />";
+		}   
+    
+    return $html;    
   }
 }
