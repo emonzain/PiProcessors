8daa367855261e2ce4dffcc07ddb8c6c2109da11
emonzain <emonzain@gmail.com>
2017-01-25 15:59:04 +0100

    Update accueil.html.php

--- views/accueil.html.php
+++ views/accueil.html.php
@@ -1,13 +1,12 @@
-<html>
-  <head>
-    <title>Welcome to My Exercise Diary</title>
-  </head>
-  <body>
-    <div>
-      <h1>Services</h1>
-    </div>
-    <p>
-    Hello Services :)
-    </p>
-  </body>
-</html>
+
+<div>
+  <h1>Services</h1>
+</div>
+<p>
+  Hello Services :)
+</p>
+<ul>
+  <li>
+    tadadaaaa
+  </li>
+</ul>
