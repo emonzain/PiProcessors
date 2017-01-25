5a7ecf6f54dd8b2b90c3d7bb872347a99b4de516
emonzain <emonzain@gmail.com>
2017-01-25 12:27:02 +0100

    Create piservice.php

---
+++ src/piservice.php
@@ -0,0 +1,36 @@
+<?php
+
+require("apiresult.php");
+
+class PiService
+{  
+    public $ServiceName = '';
+    public $ServiceStatus = '';    
+    public $ServiceDesc = '';
+    public $IsActive = false;
+    public $IsRunning = false;
+    public $IsLoaded = false;
+    
+    
+    function __construct($name)
+    {
+      $this->ServiceName = $name;
+      
+      $details = $this->get_infos();
+      
+      $data = explode("\n", trim($details));
+      $this->ServiceDesc = trim($data[0]);
+      $this->IsLoaded = substr(substr(trim($data[1]), strlen("Loaded:")), 0, strlen("loaded") === "loaded";
+      $this->IsActive = substr(substr(trim($data[2]), strlen("Active:")), 0, strlen("active") === "active";
+      $this->IsRunning = !strpos($data[2], "(running)") > 0;
+    }
+	
+    function get_infos()
+    {
+      $details = shell_exec("service ".$this->ServiceName." status");
+      
+      return $details;
+    }
+    
+    
+}
