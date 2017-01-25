d96a48ede0736e84de7c4f8f88d3caad31bbf970
emonzain <emonzain@gmail.com>
2017-01-25 15:45:02 +0100

    Create accueil.html.php

---
+++ views/accueil.html.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Welcome to My Exercise Diary</title>
+  </head>
+  <body>
+    <div>
+      <h1>Services</h1>
+    </div>
+    <p>
+    Hello Services :)
+    </p>
+  </body>
+</html>
