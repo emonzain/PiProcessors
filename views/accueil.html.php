97d6430a4a6af561351397e8b7b82705ef4166e7
emonzain <emonzain@gmail.com>
2017-01-25 17:20:52 +0100

    Update accueil.html.php

--- views/accueil.html.php
+++ views/accueil.html.php
@@ -5,8 +5,21 @@
 <p>
   Hello Services :)
 </p>
-<ul>
-  <li>
-    tadadaaaa
-  </li>
-</ul>
+<table>    
+    <tr>
+        <td>ServiceName</td>
+        <td>ServiceDesc</td>
+        <td>IsLoaded</td>
+        <td>IsActive</td>
+        <td>IsRunning</td>
+    </tr>
+<?php foreach($Services as $Service): ?>
+    <tr>
+        <td><?php echo $Service->ServiceName; ?></td>
+        <td><?php echo $Service->ServiceDesc; ?></td>
+        <td><?php echo $Service->IsLoaded; ?></td>
+        <td><?php echo $Service->IsActive; ?></td>
+        <td><?php echo $Service->IsRunning; ?></td>
+    </tr>
+<?php endforeach; ?>
+</table>
