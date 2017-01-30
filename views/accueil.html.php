e3535934bd6c7027a98e8634de6b61e014331e0b
emonzain <emonzain@gmail.com>
2017-01-30 15:44:47 +0100

    Test Cards

--- views/accueil.html.php
+++ views/accueil.html.php
@@ -1,20 +1,51 @@
 
-
-<table>    
-    <tr>
-        <td>ServiceName</td>
-        <td>ServiceDesc</td>
-        <td>IsLoaded</td>
-        <td>IsActive</td>
-        <td>IsRunning</td>
-    </tr>
+<div class="row">
+    
 <?php foreach($Model as $Service): ?>
-    <tr>
-        <td><?php echo $Service->ServiceName; ?></td>
-        <td><?php echo $Service->ServiceDesc; ?></td>
-        <td><?php echo $Service->IsLoaded; ?></td>
-        <td><?php echo $Service->IsActive; ?></td>
-        <td><?php echo $Service->IsRunning; ?></td>
-    </tr>
+
+    <div class="card">
+        <div class="card-image">
+            <span class="css_char <?php echo substr($Service->ServiceName, 0, 1); ?>">
+              <span class="inside split_vert"></span>
+              <span class="outside split_vert"></span>
+              <span class="stroke"></span>
+              <span class="fill"></span>
+            </span>
+            <span class="card-title"><?php echo $Service->ServiceName; ?></span>
+        </div>
+        <div class="card-content">
+            <p><?php echo $Service->ServiceDesc; ?></p>
+        </div>
+        <div class="card-action">
+            <a href="#"></a>
+        </div>
+    </div>    
+    
 <?php endforeach; ?>
-</table>
+    
+</div>
+
+<div class="row">
+    <table class="responsive-table stripped">   
+        <thead>
+            <tr>
+                <th data-field="Name">Name</th>
+                <th data-field="Desc">Description</th>
+                <th data-field="IsLoaded">Loaded</th>
+                <th data-field="IsActive">Active</th>
+                <th data-field="IsRunning">Running</th>
+            </tr>
+        </thead>
+<?php foreach($Model as $Service): ?>
+        <tbody>
+            <tr>
+                <td><?php echo $Service->ServiceName; ?></td>
+                <td><?php echo $Service->ServiceDesc; ?></td>
+                <td><?php echo $Service->IsLoaded; ?></td>
+                <td><?php echo $Service->IsActive; ?></td>
+                <td><?php echo $Service->IsRunning; ?></td>
+            </tr>
+        </tbody>
+<?php endforeach; ?>
+    </table>
+</div>
