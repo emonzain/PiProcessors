1c5a00cac609e77c1f5da0f2040bb93b5d351cd1
emonzain <emonzain@gmail.com>
2017-01-30 15:45:22 +0100

    Update _default_layout.php

--- views/_default_layout.php
+++ views/_default_layout.php
@@ -6,6 +6,7 @@
 	
   	<!-- Compiled and minified CSS -->
   	<link rel="stylesheet" href="/assets/css/materialize.min.css">
+  	<link rel="stylesheet" href="/assets/css/curtisfont.css">
 
   	<!-- Compiled and minified JavaScript -->
   	<script src="/assets/js/materialize.min.js"></script>	
