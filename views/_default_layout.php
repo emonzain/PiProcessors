608fe0580bd3e111416e858dbe26dbd5a1081617
Z1Zain <emonzain@gmail.com>
2017-01-30 21:54:48 +0100

    Fix css

--- views/_default_layout.php
+++ views/_default_layout.php
@@ -6,6 +6,7 @@
 	
   	<!-- Compiled and minified CSS -->
   	<link rel="stylesheet" href="/assets/css/materialize.min.css">
+  	<link rel="stylesheet" href="/assets/css/site.css">
   	<link rel="stylesheet" href="/assets/css/CurtissFont.css">
 
   	<!-- Compiled and minified JavaScript -->
