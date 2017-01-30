74b990919d7e7417c23dc011dbd5af951e66f163
emonzain <emonzain@gmail.com>
2017-01-30 16:44:04 +0100

    Create fontchar.php

---
+++ src/fontchar.php
@@ -0,0 +1,37 @@
+<?php
+
+public class FontChar
+{
+  static public function GetChar($item, $class = "")
+  {
+    $caracGen = new FontChar();
+    return "<span class='".$class." css_char ".$item."'>".$caracGen->$item."</span>";
+  }
+
+  public function a()
+  {
+    return "
+      <span class='outside split_vert'></span>
+      <span class='inside split_horiz'></span>
+      <span class='bar fill'></span>
+    ";
+  }
+  
+  public function k()
+  {
+    return "
+      <span class='outside split_vert'></span>
+      <span class='inside split_horiz'></span>
+      <span class='bar fill'></span>
+    ";
+  }
+  
+  public function p()
+  {
+    return "
+      <span class='outside split_vert'></span>
+      <span class='inside split_horiz'></span>
+      <span class='bar fill'></span>
+    ";
+  }
+}
