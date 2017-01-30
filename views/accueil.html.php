4cb583152c50d95a3ed9a9740f2d02f4c0abc745
emonzain <emonzain@gmail.com>
2017-01-30 15:51:55 +0100

    Update accueil.html.php

--- views/accueil.html.php
+++ views/accueil.html.php
@@ -2,31 +2,31 @@
 <div class="row">
     
 <?php foreach($Model as $Service): ?>
-
-    <div class="card">
-        <div class="card-image">
-            <span class="css_char <?php echo substr($Service->ServiceName, 0, 1); ?>">
-              <span class="inside split_vert"></span>
-              <span class="outside split_vert"></span>
-              <span class="stroke"></span>
-              <span class="fill"></span>
-            </span>
-            <span class="card-title"><?php echo $Service->ServiceName; ?></span>
+    <div class="col s2 m4 l6">
+        <div class="card">
+            <div class="card-image">
+                <span class="css_char <?php echo substr($Service->ServiceName, 0, 1); ?>">
+                  <span class="inside split_vert"></span>
+                  <span class="outside split_vert"></span>
+                  <span class="stroke"></span>
+                  <span class="fill"></span>
+                </span>
+                <span class="card-title"><?php echo $Service->ServiceName; ?></span>
+            </div>
+            <div class="card-content">
+                <p><?php echo $Service->ServiceDesc; ?></p>
+            </div>
+            <div class="card-action">
+                <a href="#"></a>
+            </div>
         </div>
-        <div class="card-content">
-            <p><?php echo $Service->ServiceDesc; ?></p>
-        </div>
-        <div class="card-action">
-            <a href="#"></a>
-        </div>
-    </div>    
-    
+    </div>        
 <?php endforeach; ?>
     
 </div>
 
 <div class="row">
-    <table class="responsive-table stripped">   
+    <table class="responsive-table striped">   
         <thead>
             <tr>
                 <th data-field="Name">Name</th>
