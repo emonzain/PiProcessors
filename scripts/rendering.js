4353fbff5a6a194b4c2fd70d36d559f1a0b818f3
emonzain <emonzain@gmail.com>
2017-01-31 17:23:27 +0100

    Create rendering.js

---
+++ scripts/rendering.js
@@ -0,0 +1,20 @@
+.ready(
+  function()
+  {
+    //Binding resizable items
+    $(".resizable-scale:parent").resize( function()
+      {
+        var $parent = $(this);
+        var $elem = $parent.find(".resizable-scale");
+        
+        var initSize = $elem.data("init-size");
+        $elem.css({
+          '-webkit-transform' : 'scale(' + initSize / 140 + ')',
+          '-moz-transform'    : 'scale(' + initSize / 140 + ')',
+          '-ms-transform'     : 'scale(' + initSize / 140 + ')',
+          '-o-transform'      : 'scale(' + initSize / 140 + ')',
+          'transform'         : 'scale(' + initSize / 140 + ')'
+        });
+      }    
+  }
+);
