45dd746ac509895d11db4f6558d9ec42a56a2476
emonzain <emonzain@gmail.com>
2017-02-06 16:18:45 +0100

    Update piservice.php

--- src/piservice.php
+++ src/piservice.php
@@ -36,5 +36,43 @@ class PiService
       return $details;
     }
     
+	
+	function do_action($action)
+	{
+		$appDir = option()['root_dir'];
+		$shellDirScript = $appDir."/shell-scripts/piservices-action.sh";
+		
+		$result = false;
+		$details = "";
+		switch($action)
+		{
+			case "on" : 
+				$details .= exec("echo \"Starting process : ".$this->ServiceName." ... Result : OK\" | tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
+				$details .= exec($shellDirScript." ".$this->ServiceName." start");
+				$result = true;
+				break;
+				
+			case "off" : 
+				$details .= exec("echo \"Stopping process : ".$this->ServiceName." ... Result : OK\" | tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
+				$details .= exec($shellDirScript." ".$this->ServiceName." stop");
+				$result = true;
+				break;
+			
+			default: 
+				$details .= exec("echo \"Wrong action : ".$this->ServiceName." ... Result : Error\" | tee -a /var/www/html/webcontrol-limo/actions_loging.txt");
+				$result = false;
+				break;
+		}
+		
+		
+		$apiResult = new ApiResult();
+		$apiResult->Result = $result;
+		$apiResult->Item = new PiService($this->ServiceName);
+		$apiResult->Details = $details;
+		
+		
+		return $apiResult;
+		
+	}
     
 }
