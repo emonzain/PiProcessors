6e36dad68bfa31f55997927f18d788192af62b64
emonzain <emonzain@gmail.com>
2017-08-04 16:07:11 +0200

    Update actionscripts_controller.php

--- controllers/actionscripts_controller.php
+++ controllers/actionscripts_controller.php
@@ -6,17 +6,15 @@ class ActionScriptController
 {
 	static public function ListOfActions()
 	{
-		$it = 0;
-		
 		$actionsList = array();
-		foreach($ps AS $process)
-		{
-			$it++;			
-			$newProcess = new PiActionScript($it);
 			
-			array_push($actionsList, $newProcess);
-		}
+		$newProcess = new PiActionScript("Reboot", "action_reboot.sh", "toober");
+		array_push($actionsList, $newProcess);
 			
+			
+		$newProcess = new PiActionScript("Open Door", "action_opendoor.sh", "opd");
+		array_push($actionsList, $newProcess);
+		
 		return json($actionsList);
 	}
 	
