cd46f36bf1acfbb5052a40f1ed4b5db37f739cab
emonzain <emonzain@gmail.com>
2017-08-04 15:53:18 +0200

    Start ActionsScriptsController

--- controllers/actionscripts_controller.php
+++ controllers/actionscripts_controller.php
@@ -1,1 +1,30 @@
+<?php
+
+require_once 'src/piactionscript.php';
+
+class ActionScriptController
+{
+	static public function ListOfActions()
+	{
+		$it = 0;
+		
+		$actionsList = array();
+		foreach($ps AS $process)
+		{
+			$it++;			
+			$newProcess = new PiActionScript($it);
+			
+			array_push($actionsList, $newProcess);
+		}
+			
+		return json($actionsList);
+	}
+	
+	
+	
+	static public function ListOfActions()
+	{
+		return json(ProcessController::ListOfServices());
+	}
+}
 
