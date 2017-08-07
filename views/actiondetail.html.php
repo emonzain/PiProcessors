befb75c2243284f691b52acbc28e4d8f7f32e08d
emonzain <emonzain@gmail.com>
2017-08-07 16:46:11 +0200

    Update actiondetail.html.php

--- views/actiondetail.html.php
+++ views/actiondetail.html.php
@@ -4,13 +4,17 @@ var actionDetails = new ActionScriptVM(<?php echo json_encode($Model) ?>);
 
 </script>
 
+
 <div class="row">
   <div class="action-desc col s12 m4 l2 offset-l5 offset-m4" id="action-<?php echo $ActionScript->ActionCode ?>">
+	<div>
+		<h2><?php echo $ActionScript->ActionName ?></h2>
+	  </div>
     <div class="ico-btn circle ratio square fontchar-color-red" data-bind="click: RunAction()">
     
 
-						<div class="valign center-align" style="width: 100%;" data-init-size="240">
-						  <span data-bind="visible: !Waiting()">DO</span>
+	<div class="valign center-align" style="width: 100%;" data-init-size="240">
+	      <span data-bind="visible: !Waiting()">DO</span>
               <span>
                 <i class="ico-btn material-icons" id="btnLoaded" data-bind="visible: Waiting(), css: Waiting() ? 'spinning' : '' ">loop</i>
               </span>
